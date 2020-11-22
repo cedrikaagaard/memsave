ae342ba159eea9addcd36a4d61e20525127029ec
Cedrik Aagaard <cedrikaagaard@gmail.com>
2020-11-22 23:08:04 +0100

    Small fix

--- deploy-test.sh
+++ deploy-test.sh
@@ -1,4 +1,5 @@
 echo $DEPLOY_KEY | base64 -d > deploy_key
 sudo chmod 600 deploy_key
-ssh -i deploy_key root@188.227.86.84 'reboot'
+scp -oStrictHostKeyChecking=no -i deploy_key docker-compose.yml root@188.227.86.84:/etc/docker-compose.yml/
+ssh -oStrictHostKeyChecking=no -i deploy_key root@188.227.86.84 'reboot'
 rm deploy_key
